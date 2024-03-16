5adaf41173f8fdca6abf76e94ab13c7f32fcd54a
sreek4nth <sreekanthpradeep2002@gmail.com>
2024-03-16 14:40:05 +0530

    completed

---
+++ cycle1/Product.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class ProductClass{
+	String pname;
+	int pcode;
+	int price;
+	public ProductClass(String name,int code,int price){
+		this.pname = name;
+		this.pcode = code;
+		this.price = price;
+	}
+}
+class Product {
+
+	public static void main(String[] arguments){
+		System.out.println("name:sreekanth pradeep\n roll no: 52\n date:13-02-24");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("\nEnter the code of the first product : ");
+		int code1 = scan.nextInt();
+		System.out.print("Enter the name of the first product : ");
+		scan.nextLine();
+		String name1 = scan.nextLine();
+		System.out.print("Enter the price of the first product : ");
+		int price1 = scan.nextInt();
+
+		System.out.print("\nEnter the code of the second product : ");
+		int code2 = scan.nextInt();
+		System.out.print("Enter the name of the second product : ");
+		scan.nextLine();
+		String name2 = scan.nextLine();
+		System.out.print("Enter the price of the second product : ");
+		int price2 = scan.nextInt();
+
+		System.out.print("\nEnter the code of the third product : ");
+		int code3 = scan.nextInt();
+		System.out.print("Enter the name of the third product : ");
+		scan.nextLine();
+		String name3 = scan.nextLine();
+		System.out.print("Enter the price of the third product : ");
+		int price3 = scan.nextInt();
+
+		ProductClass obj1 = new ProductClass(name1, code1, price1);
+		ProductClass obj2 = new ProductClass(name2, code2, price2);
+		ProductClass obj3 = new ProductClass(name3, code3, price3);
+
+		ProductClass minimum = obj1.price < obj2.price ? obj1 : obj2;
+		minimum = minimum.price < obj3.price ? minimum : obj3;
+		System.out.println("\nThe product with the smallest price is " + minimum.pname + " with a price of " + minimum.price + "\n");
+
+		scan.close();
+	}
+}
---
+++ cycle2/Search.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Search{
+    public static void main(String arg[]){
+		System.out.println("name:sreekanth pradeep\n roll no: 52\n date:13-02-24");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array elements : ");
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Array is : " + Arrays.toString(arr));
+        System.out.print("Enter the value to search : ");
+        int value = scanner.nextInt();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+            if(value == arr[i]){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0) System.out.println("Element " + value + " not found in the array.");
+        else System.out.println("Element " + value + " found in the array.");
+
+        scanner.close();
+    }
+}
